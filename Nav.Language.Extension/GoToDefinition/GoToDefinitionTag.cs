ba18e8129574f1ca18db5b355ae13e771b46d3db
IInspectable <github@smjh.de>
2016-05-08 11:36:25 +0200

    GoToDefinition code review

--- Nav.Language.Extension/GoToDefinition/GoToDefinitionTag.cs
+++ Nav.Language.Extension/GoToDefinition/GoToDefinitionTag.cs
@@ -3,8 +3,8 @@ using Microsoft.VisualStudio.Text.Tagging;
 
 namespace Pharmatechnik.Nav.Language.Extension.GoToDefinition {
 
-    public class GoToDefinitionTag: ITag {
-
+    public class GoToDefinitionTag: ITag, IEquatable<GoToDefinitionTag> {
+        
         public GoToDefinitionTag(string fileName) {
             if(fileName == null) {
                 throw new ArgumentNullException(nameof(fileName));
@@ -20,5 +20,40 @@ namespace Pharmatechnik.Nav.Language.Extension.GoToDefinition {
         }
         
         public Location Location { get; }
+
+        public bool Equals(GoToDefinitionTag other) {
+            if (ReferenceEquals(null, other)) {
+                return false;
+            }
+            if (ReferenceEquals(this, other)) {
+                return true;
+            }
+            return Location.Equals(other.Location);
+        }
+
+        public static bool operator ==(GoToDefinitionTag left, GoToDefinitionTag right) {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(GoToDefinitionTag left, GoToDefinitionTag right) {
+            return !Equals(left, right);
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) {
+                return false;
+            }
+            if (ReferenceEquals(this, obj)) {
+                return true;
+            }
+            if (obj.GetType() != GetType()) {
+                return false;
+            }
+            return Equals((GoToDefinitionTag)obj);
+        }
+
+        public override int GetHashCode() {
+            return Location.GetHashCode();
+        }
     }
 }
