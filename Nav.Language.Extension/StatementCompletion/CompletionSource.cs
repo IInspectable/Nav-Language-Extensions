8bc64982b8d2b1dc9242397859502d029099f4d9
IInspectable <github@smjh.de>
2016-05-17 18:09:02 +0200

    Statement completion basics

---
+++ Nav.Language.Extension/StatementCompletion/CompletionSource.cs
@@ -0,0 +1,57 @@
+﻿#region Using Directives
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Language.Intellisense;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.Extension.StatementCompletion {
+
+    internal class CompletionSource : ICompletionSource {
+
+        readonly ITextBuffer _buffer;
+        bool _disposed;
+
+        public CompletionSource(ITextBuffer buffer) {
+            _buffer = buffer;
+        }
+
+        public void AugmentCompletionSession(ICompletionSession session, IList<CompletionSet> completionSets) {
+            if (_disposed)
+                throw new ObjectDisposedException("OokCompletionSource");
+
+            List<Completion> completions = new List<Completion>()
+            {
+                new Completion("Ook!"),
+                new Completion("Ook."),
+                new Completion("Ook?")
+            };
+
+            ITextSnapshot snapshot = _buffer.CurrentSnapshot;
+            var snapshotPoint = session.GetTriggerPoint(snapshot);
+            if (snapshotPoint != null) {
+
+                var triggerPoint = (SnapshotPoint)snapshotPoint;
+
+                var line = triggerPoint.GetContainingLine();
+                SnapshotPoint start = triggerPoint;
+
+                while (start > line.Start && !char.IsWhiteSpace((start - 1).GetChar())) {
+                    start -= 1;
+                }
+
+                var applicableTo = snapshot.CreateTrackingSpan(new SnapshotSpan(start, triggerPoint), SpanTrackingMode.EdgeInclusive);
+
+                completionSets.Add(new CompletionSet("All", "All", applicableTo, completions, Enumerable.Empty<Completion>()));
+            }
+        }
+
+        public void Dispose() {
+            _disposed = true;
+        }
+    }
+}
---
+++ Nav.Language.Extension/StatementCompletion/CompletionSourceProvider.cs
@@ -0,0 +1,19 @@
+﻿#region Using Directives
+
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.Extension.StatementCompletion {
+
+    //[Export(typeof(ICompletionSourceProvider))]
+    [ContentType(NavLanguageContentDefinitions.ContentType)]
+    [Name("Nav Completion")]
+    class CompletionSourceProvider : ICompletionSourceProvider {
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer) {
+            return new CompletionSource(textBuffer);
+        }
+    }
+}
