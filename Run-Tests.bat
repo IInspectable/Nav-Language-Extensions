70ab2a917aeed76fa454393614dd563068d9a271
IInspectable <github@smjh.de>
2016-06-09 11:56:18 +0200

    Run-Tests fix

--- Run-Tests.bat
+++ Run-Tests.bat
@@ -1,5 +1,5 @@
 
 
-.\packages\NUnit.Console.3.0.1\tools\nunit3-console.exe ".\Nav.Language.Tests\bin\Debug\Nav.Language.Tests.dll" --noresult --verbose
+.\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe ".\Nav.Language.Tests\bin\Debug\Nav.Language.Tests.dll" --noresult --verbose
 
 Pause
