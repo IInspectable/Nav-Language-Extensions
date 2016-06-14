2072f82d01f1ee19488349d9f40e7971cfa5608b
IInspectable <github@smjh.de>
2016-06-14 11:43:34 +0200

    AnnotationTagNames

--- Nav.Language/CodeGen/AnnotationTagNames.cs
+++ Nav.Language/CodeGen/AnnotationTagNames.cs
@@ -2,9 +2,10 @@
 
     public static class AnnotationTagNames {
 
-        public const string TagPrefix  = "Nav";
-        public const string NavFile    = TagPrefix + "File";
-        public const string NavTask    = TagPrefix + "Task";
-        public const string NavTrigger = TagPrefix + "Trigger";
+        public const string TagPrefix   = "Nav";
+        public const string NavFile     = TagPrefix + "File";
+        public const string NavTask     = TagPrefix + "Task";
+        public const string NavTrigger  = TagPrefix + "Trigger";
+        public const string NavInitCall = TagPrefix + "InitCall";
     }
 }
