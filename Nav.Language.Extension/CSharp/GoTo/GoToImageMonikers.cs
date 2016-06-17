d77b109d2bdfc6989c60c9054b932273fa0fab9a
IInspectable <github@smjh.de>
2016-06-17 11:17:26 +0200

    GoToImageMonikers

--- Nav.Language.Extension/CSharp/GoTo/GoToImageMonikers.cs
+++ Nav.Language.Extension/CSharp/GoTo/GoToImageMonikers.cs
@@ -10,15 +10,15 @@ namespace Pharmatechnik.Nav.Language.Extension.CSharp.GoTo {
     static class GoToImageMonikers {
 
         public static ImageMoniker GoToBeginLogic {
-            get { return KnownMonikers.GoToDeclaration; }
+            get { return KnownMonikers.GoToDefinition; }
         }
 
         public static ImageMoniker GoToTriggerDefinition {
-            get { return KnownMonikers.GoToDefinition; }
+            get { return KnownMonikers.GoToDeclaration; }
         }
 
         public static ImageMoniker GoToTaskDefinition {
-            get { return KnownMonikers.GoToDefinition; }
+            get { return KnownMonikers.GoToDeclaration; }
         }
     }
 }
