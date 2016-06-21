1cf44adcd40016b74a2a919ddebcb0cb5dd1f657
IInspectable <github@smjh.de>
2016-06-21 13:49:16 +0200

    VsMenuItem

---
+++ Nav.Language.Extension/UI/VsMenuItem.cs
@@ -0,0 +1,16 @@
+﻿#region Using Directives
+
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.Extension.UI {
+
+    class VsMenuItem : MenuItem {
+
+        static VsMenuItem() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(VsMenuItem), new FrameworkPropertyMetadata(typeof(VsMenuItem)));
+        }
+    }
+}
