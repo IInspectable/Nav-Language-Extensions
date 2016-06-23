52f3976671cf138e345dafaf75c2503afe6015af
IInspectable <github@smjh.de>
2016-06-23 11:32:24 +0200

    NotificationService Wurf 1

---
+++ Nav.Language.Extension/Notification/NotificationService.cs
@@ -0,0 +1,25 @@
+﻿#region Using Directives
+
+using System;
+using Microsoft.CodeAnalysis;
+
+using Pharmatechnik.Nav.Language.CodeAnalysis.Annotation;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.Extension.Notification {
+
+    public class NavTaskAnnotationChangedArgs : EventArgs {
+
+        public DocumentId DocumentId { get; set; }
+        public NavTaskAnnotation TaskAnnotation { get; set; }
+    }
+
+    class NotificationService {
+        
+        public static void RaiseChanged(NavTaskAnnotationChangedArgs e) {
+            
+        }
+
+    }
+}
