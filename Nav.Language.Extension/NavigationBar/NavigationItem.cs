1d91f252cc72c64361dbb24fe22fb51fb6f667d0
IInspectable <github@smjh.de>
2016-07-15 19:47:59 +0200

    NavigationItem.Children

--- Nav.Language.Extension/NavigationBar/NavigationItem.cs
+++ Nav.Language.Extension/NavigationBar/NavigationItem.cs
@@ -1,18 +1,26 @@
-﻿namespace Pharmatechnik.Nav.Language.Extension.NavigationBar {
+﻿using System.Collections.Immutable;
+using JetBrains.Annotations;
+
+namespace Pharmatechnik.Nav.Language.Extension.NavigationBar {
 
     class NavigationItem {
 
-        public NavigationItem(string displayName, int imageIndex, Location location, int navigationPoint) {
-            Location    = location;
+        public NavigationItem(string displayName, int imageIndex, Location location, int navigationPoint, ImmutableList<NavigationItem> children=null) {
+            Location        = location;
             NavigationPoint = navigationPoint;
-            DisplayName = displayName;
-            ImageIndex  = imageIndex;
+            DisplayName     = displayName;
+            ImageIndex      = imageIndex;
+            Children        = children?? ImmutableList<NavigationItem>.Empty;
         }
 
         public string DisplayName { get; }
         public int ImageIndex { get; }
+        [CanBeNull]
         public Location Location { get; }
         public int NavigationPoint { get; }
+
+        [NotNull]
+        public ImmutableList<NavigationItem> Children { get; set; }
 
         public int Start {
             get { return Location?.Start ?? 0; }
--- Nav.Language.Extension/NavigationBar/TaskNavigationItemBuilder.cs
+++ Nav.Language.Extension/NavigationBar/TaskNavigationItemBuilder.cs
@@ -1,20 +1,76 @@
-﻿using System.Collections.Immutable;
+﻿#region Using Directives
+
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+#endregion
 
 namespace Pharmatechnik.Nav.Language.Extension.NavigationBar {
 
-    class TaskNavigationItemBuilder : NavigationItemBuilderBase {
-        
+    class TaskNavigationItemBuilder : SymbolVisitor {
+
+        public int TaskDefinitionImageIndex      = 0;
+        public int TaskDeclarationImageIndex     = 1;
+        public const int TriggerSymbolImageIndex = 0;
+
+        protected TaskNavigationItemBuilder() {
+            NavigationItems = new List<NavigationItem>();
+            MemberItems     = new List<NavigationItem>();
+        }
+
+        public List<NavigationItem> NavigationItems { get; }
+        public List<NavigationItem> MemberItems { get; }
+
         public static ImmutableList<NavigationItem> Build(CodeGenerationUnit codeGenerationUnit) {
-            return BuildCore(codeGenerationUnit, new TaskNavigationItemBuilder());
+
+            var builder = new TaskNavigationItemBuilder();
+
+            foreach (var symbol in codeGenerationUnit.TaskDefinitions) {
+                builder.Visit(symbol);
+            }
+
+            foreach (var symbol in codeGenerationUnit.TaskDeclarations) {
+                builder.Visit(symbol);
+            }
+
+            var items = builder.NavigationItems
+                                .OrderBy(ni => ni.Location?.Start??-1)
+                                .ToImmutableList();
+
+            return items;
         }
 
         public override void VisitTaskDefinitionSymbol(ITaskDefinitionSymbol taskDefinitionSymbol) {
-            NavigationItems.Add(new NavigationItem(taskDefinitionSymbol.Name, TaskDefinitionImageIndex, taskDefinitionSymbol.Syntax.GetLocation(), taskDefinitionSymbol.Location.Start));
+         
+            foreach (var symbol in taskDefinitionSymbol.Transitions.SelectMany(trans => trans.Symbols())) {
+                Visit(symbol);
+            }
+            
+            NavigationItems.Add(new NavigationItem(
+                displayName    : taskDefinitionSymbol.Name, 
+                imageIndex     : TaskDefinitionImageIndex, 
+                location       : taskDefinitionSymbol.Syntax.GetLocation(), 
+                navigationPoint: taskDefinitionSymbol.Location.Start,
+                children       : MemberItems.ToImmutableList()));
+
+            MemberItems.Clear();
         }
 
         public override void VisitTaskDeclarationSymbol(ITaskDeclarationSymbol taskDeclarationSymbol) {
+
+            // Haben wir bereits in Form der Taskdefinition abgefrühstückt
+            // => Jede Taskdefinition ist auch eine Deklaration
+            if(taskDeclarationSymbol.Origin == TaskDeclarationOrigin.TaskDefinition) {
+                return;
+            }
+
             // TODO Die Location der Task Deklaration sollte sich über den ganzen Syntaxbereich erstrecken...
             NavigationItems.Add(new NavigationItem(taskDeclarationSymbol.Name, TaskDeclarationImageIndex, taskDeclarationSymbol.Location, taskDeclarationSymbol.Location.Start));
+        }
+
+        public override void VisitSignalTriggerSymbol(ISignalTriggerSymbol signalTriggerSymbol) {
+            MemberItems.Add(new NavigationItem(signalTriggerSymbol.Name, TriggerSymbolImageIndex, signalTriggerSymbol.Location, signalTriggerSymbol.Start));
         }
     }
 }
