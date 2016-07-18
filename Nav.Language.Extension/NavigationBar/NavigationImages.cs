3f918055765511edaa77b35340147f27fa8a6920
IInspectable <github@smjh.de>
2016-07-18 17:48:15 +0200

    Neues TaskDeclarationImage - Feierabend checkin

--- Nav.Language.Extension/NavigationBar/NavigationImages.cs
+++ Nav.Language.Extension/NavigationBar/NavigationImages.cs
@@ -1,6 +1,8 @@
 #region 
 
+using System.Drawing;
 using System.Windows.Forms;
+
 using Microsoft.VisualStudio.Imaging;
 
 using Pharmatechnik.Nav.Language.Extension.QuickInfo;
@@ -13,20 +15,28 @@ namespace Pharmatechnik.Nav.Language.Extension.NavigationBar {
     static class NavigationImages {
 
         public static class Index {
-            public const int ProjectNode = 0;
+            public const int ProjectNode     = 0;
             public const int TaskDeclaration = 1;
-            public const int TaskDefinition = 2;
-            public const int TriggerSymbol = 3;
+            public const int TaskDefinition  = 2;
+            public const int TriggerSymbol   = 3;
         }
-        
-        public static ImageList CreateImageList() {
 
-            var imgageList = new ImageList { ImageSize = new System.Drawing.Size(16, 16) };
+        public static ImageList CreateImageList(Color backgroundColor) {
 
-            imgageList.Images.Add(NavLanguagePackage.GetBitmap(KnownMonikers.CSProjectNode));
-            imgageList.Images.Add(NavLanguagePackage.GetBitmap(SymbolImageMonikers.TaskDeclaration));
-            imgageList.Images.Add(NavLanguagePackage.GetBitmap(SymbolImageMonikers.TaskDefinition));
-            imgageList.Images.Add(NavLanguagePackage.GetBitmap(SymbolImageMonikers.SignalTrigger));
+            var baseImage = NavLanguagePackage.GetBitmap(SymbolImageMonikers.TaskDeclaration, backgroundColor);
+            var overlay   = NavLanguagePackage.GetBitmap(KnownMonikers.ReferencedElement, backgroundColor);
+
+            Bitmap taskDeclarationImage = new Bitmap(baseImage);
+            using (var grfx = Graphics.FromImage(taskDeclarationImage)) {
+                grfx.DrawImage(overlay, 0, 0);
+            }
+
+            var imgageList = new ImageList {ImageSize = new Size(16, 16)};
+
+            imgageList.Images.Add(NavLanguagePackage.GetBitmap(KnownMonikers.CSProjectNode, backgroundColor));
+            imgageList.Images.Add(taskDeclarationImage);
+            imgageList.Images.Add(NavLanguagePackage.GetBitmap(SymbolImageMonikers.TaskDefinition, backgroundColor));
+            imgageList.Images.Add(NavLanguagePackage.GetBitmap(SymbolImageMonikers.SignalTrigger, backgroundColor));
 
             return imgageList;
         }
