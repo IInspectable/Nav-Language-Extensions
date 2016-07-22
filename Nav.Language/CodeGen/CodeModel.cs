9469977f3a2fea34f14584fc3d45613da446ff6d
IInspectable <github@smjh.de>
2016-07-22 22:46:13 +0200

    CodeModel Helper Methods

--- Nav.Language/CodeGen/CodeModel.cs
+++ Nav.Language/CodeGen/CodeModel.cs
@@ -24,5 +24,39 @@ namespace Pharmatechnik.Nav.Language.CodeGen {
 
             return namespaces.ToImmutableList();
         }
+
+        public string ToCamelcase(string s) {
+
+            if(string.IsNullOrEmpty(s)) {
+                return s ?? string.Empty;
+            }
+
+            return s.Substring(0, 1).ToLowerInvariant() + s.Substring(1);
+        }
+
+        public string ToPascalcase(string s) {
+
+            if (string.IsNullOrEmpty(s)) {
+                return s ?? string.Empty;
+            }
+
+            return s.Substring(0, 1).ToUpperInvariant() + s.Substring(1);
+        }
+
+        public string ToFieldName(string s) {
+            return s?.StartsWith(FieldPräfix) == true ? ToCamelcase(s) : $"{FieldPräfix}{ToCamelcase(s)}";
+        }
+
+        public string ToClassName(string s) {
+            return ToPascalcase(s);
+        }
+
+        public string FieldPräfix {
+            get { return "_"; }
+        }
+
+        public bool IsValidIdentifier(string value) {
+            return CSharp.IsValidIdentifier(value);
+        }
     }
 }
