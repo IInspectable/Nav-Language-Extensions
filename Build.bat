692bc3d2b5b538f969551afd05c3b878eb1726b5
IInspectable <wakok@gmx.de>
2017-03-31 18:03:25 +0200

    PlatformTarget warning fix

--- Build.bat
+++ Build.bat
@@ -5,6 +5,6 @@ if "%config%" == "" (
    set config=Debug
 )
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" Nav.Language.Extensions.sln /p:Configuration="%config%"
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" Nav.Language.Extensions.sln /p:Configuration="%config%" /maxcpucount:3 /v:n
 
 Pause
