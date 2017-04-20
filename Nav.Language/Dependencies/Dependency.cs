1e9092dd3ee8da6f6a203a15ee2a6b2f2f3ecc92
IInspectable <github@smjh.de>
2017-04-20 18:14:08 +0200

    Code Review

--- Nav.Language/Dependencies/Dependency.cs
+++ Nav.Language/Dependencies/Dependency.cs
@@ -1,12 +1,4 @@
-﻿#region Using Directives
-
-using System.Linq;
-
-#endregion
-
-using System.Collections.Generic;
-
-namespace Pharmatechnik.Nav.Language.Dependencies {
+﻿namespace Pharmatechnik.Nav.Language.Dependencies {
 
     public sealed class Dependency {
         
@@ -17,18 +9,5 @@ namespace Pharmatechnik.Nav.Language.Dependencies {
 
         public DependencyItem UsingItem { get; }
         public DependencyItem UsedItem { get; }
-
-        // TODO Umbenennen und Logik wo anders hin (um welche Dependencies geht es denn?)
-        public static IEnumerable<Dependency> FromTaskDefinition(ITaskDefinitionSymbol taskDefinition) {
-
-            foreach (var taskNode in taskDefinition.NodeDeclarations
-                                                   .OfType<ITaskNodeSymbol>()
-                                                   .Where(tn=> tn.Incomings.Any() && tn.Declaration!=null)) {
-                
-                yield return new Dependency(usingItem: DependencyItem.FromSymbol(taskNode), 
-                                            usedItem : DependencyItem.FromSymbol(taskNode.Declaration));
-                
-            }
-        }
     }
 }
