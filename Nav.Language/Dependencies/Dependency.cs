57c0ea8197d76713b5eedc94569b3177f896a743
IInspectable <github@smjh.de>
2017-04-20 17:54:06 +0200

    Dependency Basics

---
+++ Nav.Language/Dependencies/Dependency.cs
@@ -0,0 +1,33 @@
+﻿#region Using Directives
+
+using System.Linq;
+
+#endregion
+
+using System.Collections.Generic;
+
+namespace Pharmatechnik.Nav.Language.Dependencies {
+
+    public sealed class Dependency {
+        
+        public Dependency(DependencyItem usingItem, DependencyItem usedItem) {
+            UsingItem = usingItem;
+            UsedItem  = usedItem;
+        }
+
+        public DependencyItem UsingItem { get; }
+        public DependencyItem UsedItem { get; }
+
+        public static IEnumerable<Dependency> FromTaskDefinition(ITaskDefinitionSymbol taskDefinition) {
+
+            foreach (var taskNode in taskDefinition.NodeDeclarations
+                                                   .OfType<ITaskNodeSymbol>()
+                                                   .Where(tn=> tn.Incomings.Any() && tn.Declaration!=null)) {
+                
+                yield return new Dependency(usingItem: DependencyItem.FromSymbol(taskNode), 
+                                            usedItem : DependencyItem.FromSymbol(taskNode.Declaration));
+                
+            }
+        }
+    }
+}
---
+++ Nav.Language/Dependencies/DependencyExtensions.cs
@@ -0,0 +1,36 @@
+﻿#region Using Directives
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.Dependencies {
+
+    public static class DependencyExtensions {
+        
+        public static Dictionary<DependencyItem, List<Dependency>> CollectIncomingDependencies(this IEnumerable<Dependency> dependencies) {
+            return Collect(dependencies, d => d.UsedItem, d => d);
+        }
+
+        public static Dictionary<DependencyItem, List<Dependency>> CollectOutgoingDependencies(this IEnumerable<Dependency> dependencies) {
+            return Collect(dependencies, d => d.UsingItem, d => d);
+        }
+
+        static Dictionary<DependencyItem, List<T>> Collect<T>(IEnumerable<Dependency> dependencies,
+                                                              Func<Dependency, DependencyItem> itemSelector, 
+                                                              Func<Dependency, T> resultSelector) {
+
+            var result = new Dictionary<DependencyItem, List<T>>();
+            foreach (var d in dependencies) {
+               
+                DependencyItem key = itemSelector(d);
+                if (!result.TryGetValue(key, out var list)) {
+                    result.Add(key, list = new List<T>());
+                }
+                list.Add(resultSelector(d));
+            }
+            return result;
+        }
+    }
+}
