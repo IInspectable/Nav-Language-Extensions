5ee833390082e7ced83a8e641bc96f9e669962b0
IInspectable <wakok@gmx.de>
2017-04-20 20:57:10 +0200

    CommandHandlerMetadata

--- Nav.Language.Extension/Commands/Infrastructure/CommandHandlerMetadata.cs
+++ Nav.Language.Extension/Commands/Infrastructure/CommandHandlerMetadata.cs
@@ -1,6 +1,8 @@
 ﻿#region Using Directives
 
+using System;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.Utilities;
 using Pharmatechnik.Nav.Language.Extension.Common;
 
 #endregion
@@ -8,11 +10,16 @@ using Pharmatechnik.Nav.Language.Extension.Common;
 namespace Pharmatechnik.Nav.Language.Extension.Commands {
 
     class CommandHandlerMetadata {
-
-        public IEnumerable<string> ContentTypes { get; }
-
+       
         public CommandHandlerMetadata(IDictionary<string, object> data) {
-            ContentTypes = (IEnumerable<string>) data.GetValueOrDefault("ContentTypes");
+            
+            ContentTypes = (IReadOnlyList<string>)data.GetValueOrDefault(nameof(ExportCommandHandlerAttribute.ContentTypes)) ?? Array.Empty<string>();
+            Before       = (IReadOnlyList<string>)data.GetValueOrDefault(nameof(OrderAttribute.Before)) ?? Array.Empty<string>();
+            After        = (IReadOnlyList<string>)data.GetValueOrDefault(nameof(OrderAttribute.After))  ?? Array.Empty<string>();
         }
+
+        public IReadOnlyList<string> ContentTypes { get; }
+        public IReadOnlyList<string> Before { get; }
+        public IReadOnlyList<string> After { get; }
     }
 }
