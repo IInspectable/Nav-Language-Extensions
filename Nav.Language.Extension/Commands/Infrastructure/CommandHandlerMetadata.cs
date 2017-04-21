f09a965f9170e600414fe874a0cd28d4082046c9
IInspectable <github@smjh.de>
2017-04-21 10:15:42 +0200

    ExtensionOrderer

--- Nav.Language.Extension/Commands/Infrastructure/CommandHandlerMetadata.cs
+++ Nav.Language.Extension/Commands/Infrastructure/CommandHandlerMetadata.cs
@@ -9,15 +9,16 @@ using Pharmatechnik.Nav.Language.Extension.Common;
 
 namespace Pharmatechnik.Nav.Language.Extension.Commands {
 
-    class CommandHandlerMetadata {
+    class CommandHandlerMetadata: IOrderableMetadata {
        
         public CommandHandlerMetadata(IDictionary<string, object> data) {
-            
+            Name         = (string) data.GetValueOrDefault(nameof(ExportCommandHandlerAttribute.Name)) ?? String.Empty;
             ContentTypes = (IReadOnlyList<string>)data.GetValueOrDefault(nameof(ExportCommandHandlerAttribute.ContentTypes)) ?? Array.Empty<string>();
             Before       = (IReadOnlyList<string>)data.GetValueOrDefault(nameof(OrderAttribute.Before)) ?? Array.Empty<string>();
             After        = (IReadOnlyList<string>)data.GetValueOrDefault(nameof(OrderAttribute.After))  ?? Array.Empty<string>();
         }
 
+        public string Name { get; }
         public IReadOnlyList<string> ContentTypes { get; }
         public IReadOnlyList<string> Before { get; }
         public IReadOnlyList<string> After { get; }
