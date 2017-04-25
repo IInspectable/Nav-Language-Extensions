3649d770cc31f31317a985ff9bf0313fbed71478
IInspectable <github@smjh.de>
2017-04-25 14:05:50 +0200

    CodeFixActionProvider Infrastructure

---
+++ Nav.Language.Extension/CodeFixes/CodeFixSuggestedActionsSourceProvider.cs
@@ -0,0 +1,34 @@
+#region Using Directives
+
+using System.ComponentModel.Composition;
+
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Language.Intellisense;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.Extension.CodeFixes {
+
+    [Export(typeof(ISuggestedActionsSourceProvider))]
+    [Name(nameof(CodeFixSuggestedActionsSourceProvider))]
+    [ContentType(NavLanguageContentDefinitions.ContentType)]
+    internal class CodeFixSuggestedActionsSourceProvider : ISuggestedActionsSourceProvider {
+
+        readonly ICodeFixActionProviderService _codeFixActionProviderService;
+
+        [ImportingConstructor]
+        public CodeFixSuggestedActionsSourceProvider(ICodeFixActionProviderService codeFixActionProviderService) {
+            _codeFixActionProviderService = codeFixActionProviderService;
+        }
+        
+        public ISuggestedActionsSource CreateSuggestedActionsSource(ITextView textView, ITextBuffer textBuffer) {
+            if (textBuffer == null && textView == null) {
+                return null;
+            }
+            // TODO nur einzelne Textbuffer unterstützen?
+            return new CodeFixSuggestedActionsSource(textBuffer, _codeFixActionProviderService);
+        }
+    }
+}
