77905aa9976b6a99f3fa43396d80c61ad4472c04
IInspectable <github@smjh.de>
2017-04-26 16:14:13 +0200

    Semantic Model um Navigationsmöglichkeiten erweitert

--- Nav.Language/Model/EdgeModeSymbol.cs
+++ Nav.Language/Model/EdgeModeSymbol.cs
@@ -1,11 +1,14 @@
 namespace Pharmatechnik.Nav.Language {
 
     sealed partial class EdgeModeSymbol : Symbol, IEdgeModeSymbol {
-        
+
+        // ReSharper disable once NotNullMemberIsNotInitialized Transition wird im Ctor der Transition während der Initialisierung gesetzt 
+        // In der "freien" Wildbahn" darf hingegen der Null Fall nicht auftreten
         public EdgeModeSymbol(string name, Location location, EdgeMode edgeMode) : base(name, location) {
             EdgeMode = edgeMode;
         }
 
         public EdgeMode EdgeMode { get;}
+        public ITransition Transition { get; set; }
     }
 }
--- Nav.Language/Model/INodeReferenceSymbol.cs
+++ Nav.Language/Model/INodeReferenceSymbol.cs
@@ -2,8 +2,16 @@
 
 namespace Pharmatechnik.Nav.Language {
 
+    public enum NodeReferenceType {
+        Source,
+        Target
+    }
+
     public interface INodeReferenceSymbol: ISymbol {
         [CanBeNull]
         INodeSymbol Declaration { get;}
+        NodeReferenceType Type { get; }
+        [NotNull]
+        ITransition Transition { get; }
     }
 }
