0ad24c1ebdc543da13485c49d36d40f0e6d1d6a4
IInspectable <github@smjh.de>
2017-04-27 11:02:31 +0200

    WhiteSpaceBetweenChoiceKeywordAndIdentifier

--- Nav.Language.Extension/CodeFixes/IntroduceChoiceAction.cs
+++ Nav.Language.Extension/CodeFixes/IntroduceChoiceAction.cs
@@ -1,7 +1,6 @@
 #region Using Directives
 
 using System;
-using System.Linq;
 using System.Threading;
 
 using Microsoft.VisualStudio.Text;
@@ -82,7 +81,7 @@ namespace Pharmatechnik.Nav.Language.Extension.CodeFixes {
                 var nodeDeclarationLine = textEdit.Snapshot.GetLineFromPosition(nodeSymbol.Start);
                 var nodeTransitionLine  = textEdit.Snapshot.GetLineFromPosition(NodeReference.End);
 
-                var choiceDeclaration = $"{GetFirstWhitespace(nodeDeclarationLine)}{SyntaxFacts.ChoiceKeyword}{WhiteSpaceBetweenKeywordAndIdentifier(nodeSymbol)}{choiceName}{SyntaxFacts.Semicolon}";
+                var choiceDeclaration = $"{GetFirstWhitespace(nodeDeclarationLine)}{SyntaxFacts.ChoiceKeyword}{WhiteSpaceBetweenChoiceKeywordAndIdentifier(nodeSymbol)}{choiceName}{SyntaxFacts.Semicolon}";
                 var choiceTransition  = $"{GetFirstWhitespace(nodeTransitionLine)}{choiceName}{WhiteSpaceBetweenSourceAndEdgeMode(edge, choiceName)}{edge.EdgeMode?.Name}{WhiteSpaceBetweenEdgeModeAndTarget(edge)}{NodeReference.Name}{SyntaxFacts.Semicolon}";
 
                 textEdit.Insert(nodeDeclarationLine.End, $"{GetNewLineCharacter()}{choiceDeclaration}");
@@ -108,71 +107,68 @@ namespace Pharmatechnik.Nav.Language.Extension.CodeFixes {
         }
 
         string WhiteSpaceBetweenSourceAndEdgeMode(IEdge edge, string newSourceName) {
-            
-            var sourceLocation = edge.Source?.Location;
-            var edgeLocation   = edge.EdgeMode?.Location;
 
-            if (sourceLocation == null || edgeLocation == null) {
+            if (edge.Source == null || edge.EdgeMode == null) {
                 return " ";
             }
             
             var sourceText = edge.ContainingTask.Syntax.SyntaxTree.SourceText;
-            var oldOffset = SpaceBetweenLocations(sourceText, sourceLocation, edgeLocation, GetTabSize()).Length;
+            var oldOffset  = ColumnsBetweenLocations(sourceText, edge.Source.Location, edge.EdgeMode.Location, GetTabSize());
 
             var oldLength = edge.Source.Location.Length;
             var newLength = newSourceName.Length;
-            var offset = Math.Max(1, oldOffset + oldLength - newLength);
+            var offset    = Math.Max(1, oldOffset + oldLength - newLength);
 
             return new String(' ', offset);
         }
 
         string WhiteSpaceBetweenEdgeModeAndTarget(IEdge edge) {
 
-            var location1 = edge.EdgeMode?.Location;
-            var location2 = edge.Target?.Location;
-
-            if (location1 == null || location2 == null) {
+            if (edge.EdgeMode == null || edge.Target == null) {
                 return " ";
             }
             var sourceText=edge.ContainingTask.Syntax.SyntaxTree.SourceText;
-            return SpaceBetweenLocations(sourceText, location1, location2, GetTabSize());
+            var offset = ColumnsBetweenLocations(sourceText, edge.EdgeMode.Location, edge.Target.Location, GetTabSize());
+            return new String(' ', offset);
         }
 
-        string WhiteSpaceBetweenKeywordAndIdentifier(INodeSymbol choice) {
-
-            var keyword = choice.Syntax.ChildTokens().First();
-            var identifier = keyword.NextToken();// choice.Syntax.ChildTokens().FirstOrMissing(SyntaxTokenClassification.Identifier);
-            if(keyword.IsMissing || identifier.IsMissing) {
+        string WhiteSpaceBetweenChoiceKeywordAndIdentifier(INodeSymbol referenceNode) {
+            
+            var locations= KeywordAndIdentifierFinder.Find(referenceNode.Syntax);
+            if (locations == null) {
                 return " ";
             }
-            // TODO Berechnen
-            return " ";
-            //var startIndex    = keyword.End;
-            //var endIndexIndex = identifier.Start;
 
-            //return choice.Syntax.SyntaxTree.SourceText.Substring(startIndex, endIndexIndex - startIndex);
+            var sourceText = referenceNode.ContainingTask.Syntax.SyntaxTree.SourceText;
+            var oldOffset = ColumnsBetweenLocations(sourceText, locations.Item1, locations.Item2, GetTabSize());
+
+            var oldLength = locations.Item1.Length;
+            var newLength = SyntaxFacts.ChoiceKeyword.Length;
+            var offset    = Math.Max(1, oldOffset + oldLength - newLength);
+
+            return new String(' ', offset);            
         }
 
-        // TODO Columns Between Locations?
-        string SpaceBetweenLocations(string sourceText, Location location1, Location location2, int tabSize) {
-            // TODO Missing?
+        int ColumnsBetweenLocations(string sourceText, Location location1, Location location2, int tabSize) {
+
             if (location1 == null || location2 == null) {
-                return String.Empty;
+                return 0;
             }
 
             int spaceCount = 1;
             if(location1.EndLine != location2.StartLine) {
-                // Symbols in unterschiedliche Zeilen                
+                // Locations in unterschiedliche Zeilen
                 var column = GetStartColumn(sourceText, location2, tabSize);
                 spaceCount = column;
             } else {
+                // Locations in selber Zeile
                 var startColumn = GetEndColumn(sourceText, location1, tabSize);
                 var endColumn   = GetStartColumn(sourceText, location2, tabSize);
 
                 spaceCount = Math.Max(1, endColumn - startColumn);
             }
 
-            return new String(' ', Math.Max(1, spaceCount));
+            return Math.Max(1, spaceCount);
         }
 
         int GetEndColumn(string sourceText, Location location, int tabSize) {
@@ -191,6 +187,51 @@ namespace Pharmatechnik.Nav.Language.Extension.CodeFixes {
             var text           = sourceText.Substring(lineStartIndex, length);
             var column         = text.GetColumnForOffset(tabSize, length);
             return column;
+        }
+
+        sealed class KeywordAndIdentifierFinder: SyntaxNodeVisitor<Tuple<Location, Location>> {
+
+            public static Tuple<Location, Location> Find(NodeDeclarationSyntax nodeDeclaration) {
+
+                var finder = new KeywordAndIdentifierFinder();
+                return finder.Visit(nodeDeclaration);
+            }
+
+            public override Tuple<Location, Location> VisitChoiceNodeDeclaration(ChoiceNodeDeclarationSyntax choiceNodeDeclarationSyntax) {
+                return SafeCreateTuple(choiceNodeDeclarationSyntax.ChoiceKeyword, choiceNodeDeclarationSyntax.Identifier);
+            }
+
+            public override Tuple<Location, Location> VisitEndNodeDeclaration(EndNodeDeclarationSyntax endNodeDeclarationSyntax) {
+                // End hat keinen Identifier
+                return DefaultVisit(endNodeDeclarationSyntax);
+            }
+
+            public override Tuple<Location, Location> VisitExitNodeDeclaration(ExitNodeDeclarationSyntax exitNodeDeclarationSyntax) {
+                return SafeCreateTuple(exitNodeDeclarationSyntax.ExitKeyword, exitNodeDeclarationSyntax.Identifier);
+            }
+
+            public override Tuple<Location, Location> VisitInitNodeDeclaration(InitNodeDeclarationSyntax initNodeDeclarationSyntax) {
+                return SafeCreateTuple(initNodeDeclarationSyntax.InitKeyword, initNodeDeclarationSyntax.Identifier);
+            }
+
+            public override Tuple<Location, Location> VisitDialogNodeDeclaration(DialogNodeDeclarationSyntax dialogNodeDeclarationSyntax) {
+                return SafeCreateTuple(dialogNodeDeclarationSyntax.DialogKeyword, dialogNodeDeclarationSyntax.Identifier);
+            }
+
+            public override Tuple<Location, Location> VisitTaskNodeDeclaration(TaskNodeDeclarationSyntax taskNodeDeclarationSyntax) {
+                return SafeCreateTuple(taskNodeDeclarationSyntax.TaskKeyword, taskNodeDeclarationSyntax.Identifier);
+            }
+
+            public override Tuple<Location, Location> VisitViewNodeDeclaration(ViewNodeDeclarationSyntax viewNodeDeclarationSyntax) {
+                return SafeCreateTuple(viewNodeDeclarationSyntax.ViewKeyword, viewNodeDeclarationSyntax.Identifier);
+            }
+            
+            Tuple<Location, Location> SafeCreateTuple(SyntaxToken token1, SyntaxToken token2) {
+                if (token1.IsMissing || token2.IsMissing) {
+                    return null;
+                }
+                return new Tuple<Location, Location>(token1.GetLocation(), token2.GetLocation());
+            }
         }
     }
 }
--- Nav.Language/Model/ISymbol.cs
+++ Nav.Language/Model/ISymbol.cs
@@ -1,9 +1,17 @@
+#region Using Directives
+
+using JetBrains.Annotations;
+
+#endregion
+
 namespace Pharmatechnik.Nav.Language {
 
     public partial interface ISymbol: IExtent {
 
         // TODO NotNull?
         string Name { get; }
+
+        [NotNull]
         Location Location { get; }
         // TODO Diagnostics hier anhängen?
     }
