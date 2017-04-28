83af56bcc3e80a8dae551e03ed51ccb2eb2e65a8
IInspectable <github@smjh.de>
2017-04-28 20:28:06 +0200

    Unit Tests + Fixes

--- Nav.Language.Tests/RenameChoiceCodeFixTests.cs
+++ Nav.Language.Tests/RenameChoiceCodeFixTests.cs
@@ -16,10 +16,11 @@ namespace Nav.Language.Tests {
     [TestFixture]
     public class RenameChoiceCodeFixTests {
 
-        [Test]
-        public void SimpleRename() {
-
-            var orgText = @"
+        public static IEnumerable<TestCaseData> TestCases = new[] {
+            //=============================================
+            //
+            CreateTestCase(
+                    originalSourceText: @"
 [namespaceprefix Test]
 
 task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
@@ -37,8 +38,8 @@ task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
     MessageBoxOK   --> Choice_Ok on Ok;
     Choice_Ok      --> Ok;
 }
-";
-            var expected = @"
+",
+                    expectedSourceText: @"
 [namespaceprefix Test]
 
 task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
@@ -56,26 +57,224 @@ task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
     MessageBoxOK   --> Choice_Renamed on Ok;
     Choice_Renamed --> Ok;
 }
-";            
-            var taskName  = "MessageBox";
-            var orgChoice = "Choice_Ok";
-            var newChoice = "Choice_Renamed";
+",
+                    taskName: "MessageBox",
+                    choiceName: "Choice_Ok",
+                    newChoiceName: "Choice_Renamed")
+                .SetName("Simple rename"),
 
-            var orgCodeGenerationUnit = GetCodeGenerationUnit(orgText);
+            //=============================================
+            //
+            CreateTestCase(
+                    originalSourceText: @"
+[namespaceprefix Test]
+
+task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
+				[result MessageBoxResult]
+{
+    init [params string message];
+        
+    view MessageBoxOK;
+    
+    exit Ok;
+    choice Choice_Ok;
+
+    init --> MessageBoxOK;  
+    
+    MessageBoxOK   --> Choice_Ok on Ok;
+    Choice_Ok      --> Ok;
+}
+",
+                    expectedSourceText: @"
+[namespaceprefix Test]
+
+task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
+				[result MessageBoxResult]
+{
+    init [params string message];
+        
+    view MessageBoxOK;
+    
+    exit Ok;
+    choice Choice_RenamedLonger;
+
+    init --> MessageBoxOK;  
+    
+    MessageBoxOK   --> Choice_RenamedLonger on Ok;
+    Choice_RenamedLonger --> Ok;
+}
+",
+                    taskName: "MessageBox",
+                    choiceName: "Choice_Ok",
+                    newChoiceName: "Choice_RenamedLonger")
+                .SetName("Rename with long choice name"),
+
+            //=============================================
+            //
+            CreateTestCase(
+                    originalSourceText: @"
+[namespaceprefix Test]
+
+task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
+				[result MessageBoxResult]
+{
+    init [params string message];
+        
+    view MessageBoxOK;
+    
+    exit Ok;
+    choice Choice_Ok;
+
+    init --> MessageBoxOK;  
+    
+    MessageBoxOK  	-->	Choice_Ok on Ok;
+    Choice_Ok		-->	Ok;
+}
+",
+                    expectedSourceText: @"
+[namespaceprefix Test]
+
+task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
+				[result MessageBoxResult]
+{
+    init [params string message];
+        
+    view MessageBoxOK;
+    
+    exit Ok;
+    choice C;
+
+    init --> MessageBoxOK;  
+    
+    MessageBoxOK  	-->	C on Ok;
+    C               -->	Ok;
+}
+",
+                    taskName: "MessageBox",
+                    choiceName: "Choice_Ok",
+                    newChoiceName: "C")
+                .SetName("Rename with short choice name"),
+
+            //=============================================
+            //
+            CreateTestCase(
+                    originalSourceText: @"
+[namespaceprefix Test]
+
+task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
+				[result MessageBoxResult]
+{
+    init [params string message];
+        
+    view MessageBoxOK;
+    
+    exit Ok;
+    choice Choice_Ok;
+
+    init --> MessageBoxOK;  
+    
+    MessageBoxOK  	    -->	Choice_Ok on Ok;
+    Choice_Ok	/* Foo*/-->	Ok;
+}
+",
+                    expectedSourceText: @"
+[namespaceprefix Test]
+
+task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
+				[result MessageBoxResult]
+{
+    init [params string message];
+        
+    view MessageBoxOK;
+    
+    exit Ok;
+    choice C;
+
+    init --> MessageBoxOK;  
+    
+    MessageBoxOK  	    -->	C on Ok;
+    C           /* Foo*/-->	Ok;
+}
+",
+                    taskName: "MessageBox",
+                    choiceName: "Choice_Ok",
+                    newChoiceName: "C")
+                .SetName("Rename with comment between source and edge"),
+
+            //=============================================
+            //
+            CreateTestCase(
+                    originalSourceText: @"
+[namespaceprefix Test]
+
+task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
+				[result MessageBoxResult]
+{
+    init [params string message];
+        
+    view MessageBoxOK;
+    
+    exit Ok;
+    choice Choice_Ok;
+
+    init --> MessageBoxOK;  
+    
+    MessageBoxOK   --> Choice_Ok on Ok;
+	Choice_Ok
+            --> Ok;
+}
+",
+                    expectedSourceText: @"
+[namespaceprefix Test]
+
+task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
+				[result MessageBoxResult]
+{
+    init [params string message];
+        
+    view MessageBoxOK;
+    
+    exit Ok;
+    choice Renamed_Choice;
+
+    init --> MessageBoxOK;  
+    
+    MessageBoxOK   --> Renamed_Choice on Ok;
+	Renamed_Choice
+            --> Ok;
+}
+",
+                    taskName: "MessageBox",
+                    choiceName: "Choice_Ok",
+                    newChoiceName: "Renamed_Choice")
+                .SetName("Rename with new line between source and edge"),
+        };
+
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void SimpleRename(string originalSourceText, string expectedSourceText, string taskName, string choiceName, string newChoiceName) {
+
+            var orgCodeGenerationUnit = GetCodeGenerationUnit(originalSourceText);
             Assert.That(orgCodeGenerationUnit.Diagnostics.Any(), Is.False, "Test Code should not have any diagnostics");
 
-            var choiceNodeSymbol = FindNodeSymbol<IChoiceNodeSymbol>(orgCodeGenerationUnit, taskName, orgChoice);
+            var choiceNodeSymbol = FindNodeSymbol<IChoiceNodeSymbol>(orgCodeGenerationUnit, taskName, choiceName);
             RenameChoiceCodeFix codeFix = new RenameChoiceCodeFix(GetEditorSettings(), orgCodeGenerationUnit, choiceNodeSymbol);
 
             Assert.That(codeFix.CanApplyFix(), Is.True);
 
-            var textChanges = codeFix.GetTextChanges(newChoice);
+            var textChanges = codeFix.GetTextChanges(newChoiceName);
 
-            var actual = ApplyChanges(orgText, textChanges);
+            var actual = ApplyChanges(originalSourceText, textChanges);
             var actualCodeGenerationUnit = GetCodeGenerationUnit(actual);
             Assert.That(actualCodeGenerationUnit.Diagnostics.Any(), Is.False, "Result Code should not have any diagnostics");
 
-            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actual, Is.EqualTo(expectedSourceText));
+        }
+
+        #region Infrastructure
+
+        static TestCaseData CreateTestCase(string originalSourceText, string expectedSourceText, string taskName, string choiceName, string newChoiceName) {
+            return new TestCaseData(originalSourceText, expectedSourceText, taskName, choiceName, newChoiceName);
         }
 
         string ApplyChanges(string text, IEnumerable<TextChange> textChanges) {
@@ -98,5 +297,7 @@ task MessageBox [base StandardWFS : ILegacyMessageBoxWFS]
             var tree=SyntaxTree.ParseText(text);
             return CodeGenerationUnit.FromCodeGenerationUnitSyntax(tree.GetRoot() as CodeGenerationUnitSyntax);
         }
+
+        #endregion
     }    
 }
