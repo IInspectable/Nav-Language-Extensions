474edd6c7bc10e4eb07e19b99f516ed6cec2f69f
IInspectable <wakok@gmx.de>
2017-04-30 21:44:58 +0200

    AndSnapshot class

--- Nav.Language.Extension/SemanticModelService/SemanticModelResult.cs
+++ Nav.Language.Extension/SemanticModelService/SemanticModelResult.cs
@@ -1,39 +1,21 @@
 ﻿#region Using Directives
 
+using System;
 using JetBrains.Annotations;
 using Microsoft.VisualStudio.Text;
+using Pharmatechnik.Nav.Language.Extension.Common;
 
 #endregion
 
 namespace Pharmatechnik.Nav.Language.Extension {
+    
+    sealed class SemanticModelResult: AndSnapshot {
 
-    sealed class SemanticModelResult {
-
-        internal SemanticModelResult([NotNull] CodeGenerationUnit codeGenerationUnit, [NotNull] ITextSnapshot snapshot) {
-            CodeGenerationUnit = codeGenerationUnit;
-            Snapshot = snapshot;
+        internal SemanticModelResult([NotNull] CodeGenerationUnit codeGenerationUnit, [NotNull] ITextSnapshot snapshot): base(snapshot) {
+            CodeGenerationUnit = codeGenerationUnit ?? throw new ArgumentNullException(nameof(codeGenerationUnit));
         }
 
         [NotNull]
-        public CodeGenerationUnit CodeGenerationUnit { get; }
-
-        [NotNull]
-        public ITextSnapshot Snapshot { get; }
-
-        public bool IsCurrent(SnapshotSpan snapshotSpan) {
-            return Snapshot.Version.VersionNumber == snapshotSpan.Snapshot.Version.VersionNumber;
-        }
-
-        public bool IsCurrent(ITextSnapshot snapshot) {
-            // TODO ReiteratedVersionNumber verwenden? => Undo würde "optimiert"
-            if(snapshot != Snapshot) {
-                return false;
-            }
-            return Snapshot.Version.VersionNumber == snapshot.Version.VersionNumber;
-        }
-
-        public bool IsCurrent(ITextBuffer textBuffer) {
-            return IsCurrent(textBuffer.CurrentSnapshot);
-        }        
+        public CodeGenerationUnit CodeGenerationUnit { get; }         
     }
 }
