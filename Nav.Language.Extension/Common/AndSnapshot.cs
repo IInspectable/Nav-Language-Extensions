3a3e192be10148e8172da7d652adf06c87f8e1a4
IInspectable <github@smjh.de>
2017-04-30 21:44:58 +0200

    AndSnapshot class

---
+++ Nav.Language.Extension/Common/AndSnapshot.cs
@@ -0,0 +1,37 @@
+﻿#region Using Directives
+
+using System;
+
+using JetBrains.Annotations;
+
+using Microsoft.VisualStudio.Text;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.Extension.Common {
+    abstract class AndSnapshot {
+
+        protected AndSnapshot([NotNull] ITextSnapshot snapshot) {
+            Snapshot = snapshot ?? throw new ArgumentNullException(nameof(snapshot));
+        }
+
+        [NotNull]
+        public ITextSnapshot Snapshot { get; }
+
+        public bool IsCurrent(SnapshotSpan snapshotSpan) {
+            return Snapshot.Version.VersionNumber == snapshotSpan.Snapshot.Version.VersionNumber;
+        }
+
+        public bool IsCurrent(ITextSnapshot snapshot) {
+            // TODO ReiteratedVersionNumber verwenden? => Undo würde "optimiert"
+            if (snapshot != Snapshot) {
+                return false;
+            }
+            return Snapshot.Version.VersionNumber == snapshot.Version.VersionNumber;
+        }
+
+        public bool IsCurrent(ITextBuffer textBuffer) {
+            return IsCurrent(textBuffer.CurrentSnapshot);
+        }
+    }
+}
