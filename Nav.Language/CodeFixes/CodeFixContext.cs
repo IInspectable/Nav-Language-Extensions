a7ebc75ffad81dd11e1d9ef0220dd5f33ff7d2a2
IInspectable <github@smjh.de>
2017-05-05 15:02:24 +0200

    Review

--- Nav.Language/CodeFixes/CodeFixContext.cs
+++ Nav.Language/CodeFixes/CodeFixContext.cs
@@ -3,55 +3,43 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 
 #endregion
 
 namespace Pharmatechnik.Nav.Language.CodeFixes {
     public sealed class CodeFixContext {
 
-        public CodeFixContext(int position, CodeGenerationUnit codeGenerationUnit, EditorSettings editorSettings) {
-            // TODO Verify, Unit Tests
-
-            Position           = position;
+        public CodeFixContext(TextExtent extent, CodeGenerationUnit codeGenerationUnit, EditorSettings editorSettings) {
+            
             CodeGenerationUnit = codeGenerationUnit ?? throw new ArgumentNullException(nameof(codeGenerationUnit));
             EditorSettings     = editorSettings     ?? throw new ArgumentNullException(nameof(editorSettings));
-            LineExtent         = codeGenerationUnit.Syntax.SyntaxTree.GetTextLineExtent(position);                       
+            Range              = extent;
+
+            // TODO Verify, Unit Tests
         }
 
-        public int Position { get; }
-        public TextLineExtent LineExtent { get; }
+        public TextExtent Range { get; }
         public CodeGenerationUnit CodeGenerationUnit { get; }
         public EditorSettings EditorSettings { get; }
-
-        [CanBeNull]
-        public ISymbol TryFindSymbolAtPosition() {
-            var symbol = CodeGenerationUnit.Symbols.FindAtPosition(Position);
-            if (symbol == null && Position > LineExtent.Extent.Start) {
-                symbol = CodeGenerationUnit.Symbols.FindAtPosition(Position - 1);
-            }
-            return symbol;
+        
+        public IEnumerable<ISymbol> FindSymbols(bool includeOverlapping = false) {
+            return CodeGenerationUnit.Symbols[Range];
         }
 
-        [CanBeNull]
-        public SyntaxNode TryFindNodeAtPosition() {
-            var token = CodeGenerationUnit.Syntax.SyntaxTree.Tokens.FindAtPosition(Position);
-            if (token.Classification == SyntaxTokenClassification.Whitespace && Position > LineExtent.Extent.Start) {
-                token = CodeGenerationUnit.Syntax.SyntaxTree.Tokens.FindAtPosition(Position - 1);
-            }
-            return token.Parent;
+        public IEnumerable<T> FindSymbols<T>(bool includeOverlapping = false) where T : ISymbol {
+            return FindSymbols().OfType<T>();
         }
 
         public IEnumerable<T> FindNodes<T>(bool includeOverlapping = false) where T : SyntaxNode {
             var candidates = FindTokens().Select(t => t.Parent).OfType<T>();
             if (!includeOverlapping) {
-                return candidates.Where(node => LineExtent.Extent.IntersectsWith(node.Extent));
+                return candidates.Where(node => Range.IntersectsWith(node.Extent));
             }
             return candidates;
         }
 
         public IEnumerable<SyntaxToken> FindTokens() {
-            return CodeGenerationUnit.Syntax.SyntaxTree.Tokens[LineExtent.Extent];
+            return CodeGenerationUnit.Syntax.SyntaxTree.Tokens[Range];
         }
 
         public bool ContainsNodes<T>() where T : SyntaxNode {
