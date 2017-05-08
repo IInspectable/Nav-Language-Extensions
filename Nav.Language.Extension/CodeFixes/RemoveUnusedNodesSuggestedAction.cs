7f1325a4337fc248fbc3b456380ebec587b3c7fd
IInspectable <github@smjh.de>
2017-05-08 14:42:56 +0200

    RemoveUnusedNodesCodeFix Wurf 1

---
+++ Nav.Language.Extension/CodeFixes/RemoveUnusedNodesSuggestedAction.cs
@@ -0,0 +1,29 @@
+﻿#region Using Directives
+
+using System.Threading;
+
+using Microsoft.VisualStudio.Imaging.Interop;
+using Pharmatechnik.Nav.Language.CodeFixes;
+using Pharmatechnik.Nav.Language.Extension.Images;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.Extension.CodeFixes {
+
+    class RemoveUnusedNodesSuggestedAction : CodeFixSuggestedAction<RemoveUnusedNodesCodeFix> {
+
+        public RemoveUnusedNodesSuggestedAction(RemoveUnusedNodesCodeFix codeFix,
+                                                CodeFixSuggestedActionParameter parameter,
+                                                CodeFixSuggestedActionContext context)
+            : base(context, parameter, codeFix) {
+        }
+
+        public override ImageMoniker IconMoniker => ImageMonikers.RemoveUnusedNodes;
+        public override string DisplayText       => CodeFix.Name;
+
+        protected override void Apply(CancellationToken cancellationToken) {
+
+            ApplyTextChanges(CodeFix.GetTextChanges());                
+        }
+    }
+}
---
+++ Nav.Language/CodeFixes/RemoveUnusedNodesCodeFixProvider.cs
@@ -0,0 +1,42 @@
+#region Using Directives
+
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.CodeFixes {
+
+    public sealed class RemoveUnusedNodesCodeFixProvider {
+
+        public static IEnumerable<RemoveUnusedNodesCodeFix> SuggestCodeFixes(CodeFixContext context, CancellationToken cancellationToken) {
+
+            // Wir schlagen den Codefix nur vor, wenn sich das Caret in einer Node Declaration befindet
+            var nodeDeclarationSyntax = context.FindNodes<NodeDeclarationSyntax>().FirstOrDefault();
+
+            // Von dieser Node Declaration laufen wir hoch zum ganzen Block
+            var nodeDeclarationBlock = nodeDeclarationSyntax?.AncestorsAndSelf().OfType<NodeDeclarationBlockSyntax>().FirstOrDefault();
+            if (nodeDeclarationBlock == null) {
+                yield break;
+            }
+
+            // Und von dort zur Task Definition
+            var taskDefinition = context.CodeGenerationUnit.TaskDefinitions.FirstOrDefault(td => td.Syntax.NodeDeclarationBlock == nodeDeclarationBlock);
+            if (taskDefinition == null) {
+                // Sollte eigentlich nicht möglich sein
+                yield break;
+            }
+            var codeFix = new RemoveUnusedNodesCodeFix(taskDefinition, context);
+            if (codeFix.CanApplyFix()) {
+                yield return codeFix;
+            }
+
+
+        //  return context.FindSymbols<ITaskDefinitionSymbol>()
+        //                .Select(taskDefinition => new RemoveUnusedNodesCodeFix(taskDefinition, context))
+        //                .Where(codeFix => codeFix.CanApplyFix());
+        //
+        }
+    }
+}
