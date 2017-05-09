a7f1a1384ffedf3d501264f01bc394b63e4580c1
IInspectable <github@smjh.de>
2017-05-09 19:49:43 +0200

    Test Fix

--- Nav.Language.Tests/SyntaxNodeTriviaTests.cs
+++ Nav.Language.Tests/SyntaxNodeTriviaTests.cs
@@ -1,6 +1,5 @@
 ﻿#region Using Directives
 
-using System;
 using NUnit.Framework;
 using Pharmatechnik.Nav.Language;
 
@@ -11,6 +10,15 @@ namespace Nav.Language.Tests {
 
     [TestFixture]
     public class SyntaxNodeTriviaTests {
+
+        int _nlCharCount;
+        [SetUp]
+        public void Setup() {
+            // Merkwürdigerweise haben beim Compile auf Appveyor die NLs nur eine Länge von 1...
+            var nlTestString = @"
+";
+            _nlCharCount = nlTestString.Length;
+        }
 
         // TODO Weitere Tests für Trivias
         [Test]
@@ -23,23 +31,23 @@ namespace Nav.Language.Tests {
     task B; //Comment
 task C;
 ";
-            Assert.That(Environment.NewLine.Length, Is.EqualTo(2), "Environment.NewLine");
+            Assert.That(_nlCharCount, Is.EqualTo(2), "Environment.NewLine");
             var ndb= Syntax.ParseNodeDeclarationBlock(source);
 
             var taskA=ndb.NodeDeclarations[0];
 
-            Assert.That(taskA.GetLeadingTriviaExtent(), Is.EqualTo(new TextExtent(0, length: 11)));
-            Assert.That(taskA.GetTrailingTriviaExtent(), Is.EqualTo(new TextExtent(18, length: 2))); // NewLine!!
+            Assert.That(taskA.GetLeadingTriviaExtent(), Is.EqualTo(new TextExtent(0, length: 9+_nlCharCount)));
+            Assert.That(taskA.GetTrailingTriviaExtent(), Is.EqualTo(new TextExtent(18, length: _nlCharCount)));
 
             var taskB = ndb.NodeDeclarations[1];
 
             Assert.That(taskB.GetLeadingTriviaExtent(), Is.EqualTo(new TextExtent(20, length: 4+2+14)));
-            Assert.That(taskB.GetTrailingTriviaExtent(), Is.EqualTo(new TextExtent(47, length: 12))); // NewLine!!
+            Assert.That(taskB.GetTrailingTriviaExtent(), Is.EqualTo(new TextExtent(47, length: 10+_nlCharCount)));
 
             var taskC = ndb.NodeDeclarations[2];
 
             Assert.That(taskC.GetLeadingTriviaExtent(),  Is.EqualTo(new TextExtent(59, length:0)));
-            Assert.That(taskC.GetTrailingTriviaExtent(), Is.EqualTo(new TextExtent(66, length: 2))); // NewLine!!
+            Assert.That(taskC.GetTrailingTriviaExtent(), Is.EqualTo(new TextExtent(66, length: _nlCharCount))); 
         }
 
         [Test]
@@ -62,7 +70,7 @@ task C;
             var taskC = ndb.NodeDeclarations[2];
             //
             Assert.That(taskC.GetLeadingTriviaExtent(), Is.EqualTo(new TextExtent(47, length: 0)));
-            Assert.That(taskC.GetTrailingTriviaExtent(), Is.EqualTo(new TextExtent(54, length: 2))); // NewLine!!
+            Assert.That(taskC.GetTrailingTriviaExtent(), Is.EqualTo(new TextExtent(54, length: _nlCharCount)));
         }
 
         [Test]
@@ -80,7 +88,7 @@ task C;
             var taskA = ndb.NodeDeclarations[0];
 
             Assert.That(taskA.GetLeadingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(7, length: 4)));
-            Assert.That(taskA.GetTrailingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(18, length: 2))); // NewLine!!
+            Assert.That(taskA.GetTrailingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(18, length: _nlCharCount)));
 
             var taskB = ndb.NodeDeclarations[1];
 
@@ -90,7 +98,7 @@ task C;
             var taskC = ndb.NodeDeclarations[2];
 
             Assert.That(taskC.GetLeadingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(59, length: 0)));
-            Assert.That(taskC.GetTrailingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(66, length: 2))); // NewLine!!
+            Assert.That(taskC.GetTrailingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(66, length: _nlCharCount)));
         }
 
         [Test]
@@ -113,7 +121,7 @@ task C;
             var taskC = ndb.NodeDeclarations[2];
             //
             Assert.That(taskC.GetLeadingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(47, length: 0)));
-            Assert.That(taskC.GetTrailingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(54, length: 2))); // NewLine!!
+            Assert.That(taskC.GetTrailingTriviaExtent(onlyWhiteSpace: true), Is.EqualTo(new TextExtent(54, length: _nlCharCount))); 
         }
     }
 }
