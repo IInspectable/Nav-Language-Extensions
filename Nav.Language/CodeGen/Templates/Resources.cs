da00503e62d201478d8d36a4e2ffe73ac33eb266
IInspectable <github@smjh.de>
2017-05-10 13:44:22 +0200

    CodeGenerator Kleinkram

---
+++ Nav.Language/CodeGen/Templates/Resources.cs
@@ -0,0 +1,25 @@
+﻿#region Using Directives
+
+using System.IO;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.CodeGen.Templates {
+
+    static class Resources {
+        
+        public static string BeginWFSTemplate => LoadText("IBeginWFS.stg");
+
+        static string LoadText(string resourceName) {
+
+            var fullResourceName = $"{typeof(Resources).Namespace}.{resourceName}";
+
+            using (Stream stream = typeof(Resources).Assembly.GetManifestResourceStream(fullResourceName)) 
+            // ReSharper disable once AssignNullToNotNullAttribute Lass krachen...
+            using (StreamReader reader = new StreamReader(stream)) {
+                string result = reader.ReadToEnd();
+                return result;
+            }
+        }
+    }
+}
