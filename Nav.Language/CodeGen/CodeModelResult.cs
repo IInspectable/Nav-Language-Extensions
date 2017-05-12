daadb2515e02e84dcaa9f6e96a0dc8cf94612b51
IInspectable <github@smjh.de>
2017-05-12 14:49:36 +0200

    CodeGenerator Refactoring

---
+++ Nav.Language/CodeGen/CodeModelResult.cs
@@ -0,0 +1,30 @@
+#region Using Directives
+
+using System;
+using JetBrains.Annotations;
+
+#endregion
+
+namespace Pharmatechnik.Nav.Language.CodeGen {
+
+    sealed class CodeModelResult {
+
+        public CodeModelResult(ITaskDefinitionSymbol taskDefinition, IBeginWfsCodeModel beginWfsCodeModel, IWfsCodeModel wfsCodeModel, WfsBaseCodeModel wfsBaseCodeModel) {
+            TaskDefinition     = taskDefinition    ?? throw new ArgumentNullException(nameof(taskDefinition));
+            IBeginWfsCodeModel = beginWfsCodeModel ?? throw new ArgumentNullException(nameof(beginWfsCodeModel));
+            IWfsCodeModel      = wfsCodeModel      ?? throw new ArgumentNullException(nameof(wfsCodeModel));
+            WfsBaseCodeModel   = wfsBaseCodeModel  ?? throw new ArgumentNullException(nameof(wfsCodeModel));
+        }
+
+        // ReSharper disable InconsistentNaming
+        [NotNull]
+        public ITaskDefinitionSymbol TaskDefinition { get; }
+        [NotNull]
+        public IBeginWfsCodeModel IBeginWfsCodeModel { get; }
+        [NotNull]
+        public IWfsCodeModel IWfsCodeModel { get; }
+        [NotNull]
+        public WfsBaseCodeModel WfsBaseCodeModel { get; }
+        // ReSharper restore InconsistentNaming
+    }
+}
