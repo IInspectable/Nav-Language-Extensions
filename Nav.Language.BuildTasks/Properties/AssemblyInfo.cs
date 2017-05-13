c9b909faba0e38596c7141df4c8bd938a14cb785
IInspectable <wakok@gmx.de>
2017-05-13 13:32:33 +0200

    AssemblyInfo Update

--- Nav.Language.BuildTasks/Properties/AssemblyInfo.cs
+++ Nav.Language.BuildTasks/Properties/AssemblyInfo.cs
@@ -1,35 +1,18 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using Nav.Language.BuildTasks;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Nav.Language.Service")]
+using System.Reflection;
+using System.Runtime.InteropServices;
+// TODO Assembly Infos bereitstellen
+[assembly: AssemblyTitle(ThisAssembly.ProductName)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nav.Language.Service")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct(ThisAssembly.ProductName)]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e07ed6f-4fc9-47da-96c3-bcd06d583bc9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(ThisAssembly.ProductVersion)]
+[assembly: AssemblyFileVersion(ThisAssembly.ProductVersion)]
--- Nav.Language.ServiceHost/Properties/AssemblyInfo.cs
+++ Nav.Language.ServiceHost/Properties/AssemblyInfo.cs
@@ -1,26 +1,19 @@
-﻿using System.Reflection;
+﻿using Nav.Language.ServiceHost;
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-using Nav.Language.ServiceHost;
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// TODO Assembly Infos bereitstellen
 [assembly: AssemblyTitle(ThisAssembly.ProductName)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct(ThisAssembly.ProductName)]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e248a2f-c50a-492a-b8ca-282e5dee988e")]
 
 [assembly: AssemblyVersion(ThisAssembly.ProductVersion)]
 [assembly: AssemblyFileVersion(ThisAssembly.ProductVersion)]
