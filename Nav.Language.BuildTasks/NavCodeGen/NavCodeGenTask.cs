35b6ee7f3a0a12a6c1deddbc33cc8d709881e61c
IInspectable <wakok@gmx.de>
2017-05-13 13:14:13 +0200

    NavCodeGenTask Infrastructure

---
+++ Nav.Language.BuildTasks/NavCodeGen/NavCodeGenTask.cs
@@ -0,0 +1,37 @@
+﻿#region Using Directives
+
+using System.Linq;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+#endregion
+
+namespace Nav.Language.BuildTasks {
+
+    public class NavCodeGenTask: Task {
+
+        public ITaskItem[] Files { get; set; }
+
+        public override bool Execute() {
+
+            WriteHeader();
+
+            var files = Files.Select(item => item.ItemSpec);
+            foreach(var file in files) {
+                Log.LogWarning($"Nav: '{file}'");      
+                //Log.LogMessage(MessageImportance.Low, $"Nav: '{file}'");
+            }
+            
+            return true;
+        }
+
+        void WriteHeader() {
+            var productInfo = $"{ThisAssembly.ProductName} Version {ThisAssembly.ProductVersion}";
+            var separator = new string('=', productInfo.Length);
+            Log.LogMessage(separator);
+            Log.LogMessage(productInfo);
+            Log.LogMessage(separator);
+        }
+    }
+}
