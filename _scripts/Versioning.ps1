d5b4acb33fe76c5ed36554e58166299607b8b57e
IInspectable <github@smjh.de>
2017-05-13 12:15:50 +0200

    Nav.Language.BuildTasks Project

--- _scripts/Versioning.ps1
+++ _scripts/Versioning.ps1
@@ -5,6 +5,7 @@ $targetFiles=
     "$PSScriptRoot'\..\Nav.Language.ServiceHost\UpdateProductVersion.targets",
     "$PSScriptRoot'\..\Nav.Language.CodeAnalysis\UpdateProductVersion.targets",
     "$PSScriptRoot'\..\Nav.Language\UpdateProductVersion.targets",
+	"$PSScriptRoot'\..\Nav.Language.BuildTasks\UpdateProductVersion.targets",
     "$PSScriptRoot'\..\Nav.Utilities\UpdateProductVersion.targets"
 
 function IncreaseMajor(){
