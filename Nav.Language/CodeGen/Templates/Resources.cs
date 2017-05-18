0d2fa027331e72dc36ccc59c7def8d6d87a29352
IInspectable <github@smjh.de>
2017-05-18 20:58:58 +0200

    Resource loading optimized

--- Nav.Language/CodeGen/Templates/Resources.cs
+++ Nav.Language/CodeGen/Templates/Resources.cs
@@ -8,11 +8,11 @@ namespace Pharmatechnik.Nav.Language.CodeGen.Templates {
 
     static class Resources {
         // ReSharper disable InconsistentNaming
-        public static string IBeginWfsTemplate  => LoadText("IBeginWFS.stg");
-        public static string IWfsTemplate       => LoadText("IWFS.stg");
-        public static string WfsBaseTemplate    => LoadText("WFSBase.stg");
-        public static string WFSOneShotTemplate => LoadText("WFSOneShot.stg");
-        public static string CommonTemplate     => LoadText("Common.stg");
+        public static readonly string IBeginWfsTemplate  = LoadText("IBeginWFS.stg");
+        public static readonly string IWfsTemplate       = LoadText("IWFS.stg");
+        public static readonly string WfsBaseTemplate    = LoadText("WFSBase.stg");
+        public static readonly string WFSOneShotTemplate = LoadText("WFSOneShot.stg");
+        public static readonly string CommonTemplate     = LoadText("Common.stg");
         // ReSharper restore InconsistentNaming
 
         static string LoadText(string resourceName) {
