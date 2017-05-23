f6a909f5bfa6c2fe855f97ce321e0a37955373c7
IInspectable <github@smjh.de>
2017-05-23 13:52:24 +0200

    One Shot

--- Nav.Language/CodeGen/CodeModel/WfsCodeModel.cs
+++ Nav.Language/CodeGen/CodeModel/WfsCodeModel.cs
@@ -11,25 +11,36 @@ namespace Pharmatechnik.Nav.Language.CodeGen {
     sealed class WfsCodeModel : FileGenerationCodeModel {
 
         public WfsCodeModel(
-               TaskCodeModel taskCodeModel, 
-               string relativeSyntaxFileName, 
-               string filePath,
-               ImmutableList<string> usingNamespaces)
+            TaskCodeModel taskCodeModel, 
+            string relativeSyntaxFileName, 
+            string filePath,
+            ImmutableList<string> usingNamespaces,
+            ImmutableList<InitTransitionCodeModel> initTransitions,
+            ImmutableList<ExitTransitionCodeModel> exitTransitions,
+            ImmutableList<TriggerTransitionCodeModel> triggerTransitions)
             : base(taskCodeModel, relativeSyntaxFileName, filePath) {
 
-            UsingNamespaces = usingNamespaces ?? throw new ArgumentNullException(nameof(usingNamespaces));
+            UsingNamespaces    = usingNamespaces    ?? throw new ArgumentNullException(nameof(usingNamespaces));
+            InitTransitions    = initTransitions    ?? throw new ArgumentNullException(nameof(initTransitions));
+            ExitTransitions    = exitTransitions    ?? throw new ArgumentNullException(nameof(exitTransitions));
+            TriggerTransitions = triggerTransitions ?? throw new ArgumentNullException(nameof(triggerTransitions));
         }
 
         public string WflNamespace => Task.WflNamespace;
         public string WfsTypeName  => Task.WfsTypeName;
 
         public ImmutableList<string> UsingNamespaces { get; }
-        
+        public ImmutableList<InitTransitionCodeModel> InitTransitions { get; }
+        public ImmutableList<ExitTransitionCodeModel> ExitTransitions { get; }
+        public ImmutableList<TriggerTransitionCodeModel> TriggerTransitions { get; }
+
         public static WfsCodeModel FromTaskDefinition(ITaskDefinitionSymbol taskDefinition, IPathProvider pathProvider) {
 
             if (taskDefinition == null) {
                 throw new ArgumentNullException(nameof(taskDefinition));
             }
+
+            var baseCodeModel=WfsBaseCodeModel.FromTaskDefinition(taskDefinition, pathProvider);
 
             var taskCodeModel = TaskCodeModel.FromTaskDefinition(taskDefinition);
             var relativeSyntaxFileName = pathProvider.GetRelativePath(pathProvider.WfsFileName, pathProvider.SyntaxFileName);
@@ -38,7 +49,10 @@ namespace Pharmatechnik.Nav.Language.CodeGen {
                 taskCodeModel         : taskCodeModel,
                 relativeSyntaxFileName: relativeSyntaxFileName,
                 filePath              : pathProvider.WfsFileName,
-                usingNamespaces       : GetUsingNamespaces(taskDefinition, taskCodeModel).ToImmutableList()
+                usingNamespaces       : GetUsingNamespaces(taskDefinition, taskCodeModel).ToImmutableList(),
+                initTransitions       : baseCodeModel.InitTransitions,
+                exitTransitions       : baseCodeModel.ExitTransitions,
+                triggerTransitions    : baseCodeModel.TriggerTransitions
                );
         }
 
