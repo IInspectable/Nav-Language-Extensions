cad73a24f8b49bfaf98914b255e81dfc78e94dc9
IInspectable <github@smjh.de>
2018-02-23 10:38:25 +0100

    Regressionstest

---
+++ Nav.Language.Tests/Regression/Tests/WFL/TestWFS.expected.cs
@@ -0,0 +1,19 @@
+﻿#region Using Directives
+using System;
+using Nav.Language.Tests.Regression.Test1.IWFL;
+using Pharmatechnik.Apotheke.XTplus.Common.WFL;
+using Pharmatechnik.Apotheke.XTplus.Common.IWFL;
+using Pharmatechnik.Apotheke.XTplus.Framework.Core.WFL;
+using Pharmatechnik.Apotheke.XTplus.Framework.Core.IWFL;
+using Pharmatechnik.Apotheke.XTplus.Framework.NavigationEngine.WFL;
+using Pharmatechnik.Apotheke.XTplus.Framework.NavigationEngine.IWFL;
+#endregion
+
+namespace Nav.Language.Tests.Regression.Test1.WFL {	
+    public partial class TestWFS {
+        protected override INavCommandBody BeginLogic() {
+            throw new NotImplementedException();
+        }
+
+    }
+}
