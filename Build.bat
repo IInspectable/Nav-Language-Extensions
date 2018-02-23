c259da32b5dcbdbe1c02964b26fa88c9e80f6ae9
IInspectable <github@smjh.de>
2018-02-23 20:38:03 +0100

    nuget restore

--- Build.bat
+++ Build.bat
@@ -5,6 +5,7 @@ if "%config%" == "" (
    set config=Debug
 )
 
+nuget restore
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" Nav.Language.Extensions.sln /p:Configuration="%config%" /maxcpucount:4 /v:n
 
 REM Pause
