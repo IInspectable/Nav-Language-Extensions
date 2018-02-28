1a24ba2e00bffcb41d9e0075bea353872c3949f2
IInspectable <github@smjh.de>
2018-02-28 14:15:43 +0100

    Analyzer

---
+++ Nav.Language/SemanticAnalyzer/Nav0023AnOutgoingEdgeForTrigger0IsAlreadyDeclared.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Pharmatechnik.Nav.Language.SemanticAnalyzer {
+
+    public class Nav0023AnOutgoingEdgeForTrigger0IsAlreadyDeclared: ITaskDefinitionAnalyzer {
+
+        public DiagnosticDescriptor Descriptor => DiagnosticDescriptors.Semantic.Nav0023AnOutgoingEdgeForTrigger0IsAlreadyDeclared;
+
+        public IEnumerable<Diagnostic> Analyze(ITaskDefinitionSymbol taskDefinition, AnalyzerContext context) {
+            //==============================
+            // An outgoing edge for Trigger '{0}' is already declared
+            //==============================
+            var triggerMap = new Dictionary<INodeSymbol, ITriggerSymbol>();
+            foreach (var trans in taskDefinition.TriggerTransitions) {
+                // Nicht deklarierte Sourcenodes interessieren uns nicht
+                var nodeSymbol = trans.SourceReference?.Declaration;
+                if (nodeSymbol == null) {
+                    continue;
+                }
+
+                triggerMap.TryGetValue(nodeSymbol, out var existing);
+
+                foreach (var trigger in trans.Triggers) {
+
+                    if (existing != null && trigger.Name == existing.Name) {
+
+                        yield return (new Diagnostic(
+                            trigger.Location,
+                            existing.Location,
+                            Descriptor,
+                            existing.Name));
+
+                    } else {
+                        triggerMap[nodeSymbol] = trigger;
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Nav.Language/SemanticAnalyzer/Nav0110Edge0NotAllowedIn1BecauseItsReachableFromInit2.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Pharmatechnik.Nav.Language.SemanticAnalyzer {
+
+    public class Nav0110Edge0NotAllowedIn1BecauseItsReachableFromInit2: ITaskDefinitionAnalyzer {
+
+        public DiagnosticDescriptor Descriptor => DiagnosticDescriptors.Semantic.Nav0110Edge0NotAllowedIn1BecauseItsReachableFromInit2;
+
+        public IEnumerable<Diagnostic> Analyze(ITaskDefinitionSymbol taskDefinition, AnalyzerContext context) {
+            //==============================
+            // '{0}' edge not allowed here because '{1}' is reachable from init node '{2}'
+            //==============================
+            foreach (var initNode in taskDefinition.NodeDeclarations.OfType<IInitNodeSymbol>()) {
+
+                // Interessanterweise darf eine Init-Transition merhr als einen Ausgang haben, und hat somit
+                // eine "eingebaute choice".
+                foreach (var initTransition in initNode.Outgoings) {
+                    foreach (var reachableCall in initTransition.GetReachableCalls()
+                                                                .Where(c => c.EdgeMode.EdgeMode != EdgeMode.Goto)) {
+                        yield return new Diagnostic(
+                            reachableCall.EdgeMode.Location,
+                            DiagnosticDescriptors.Semantic.Nav0110Edge0NotAllowedIn1BecauseItsReachableFromInit2,
+                            reachableCall.EdgeMode.DisplayName,
+                            reachableCall.Node.Name,
+                            initNode.Name);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
