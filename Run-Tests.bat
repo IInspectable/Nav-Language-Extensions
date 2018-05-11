7d3e88c532399e1e5c4e4a8cdcc00e7564cd69ec
IInspectable <github@smjh.de>
2018-05-11 13:19:00 +0200

    Run-Build fix

--- Run-Tests.bat
+++ Run-Tests.bat
@@ -1,5 +1,5 @@
 @Echo Off
 
-.\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe ".\Nav.Language.Tests\bin\Debug\Nav.Language.Tests.dll" ".\Nav.Language.Extension.Tests\bin\Debug\Nav.Language.Extension.Tests.dll"
+.\_build\NUnit.ConsoleRunner\3.8.0\tools\nunit3-console.exe ".\Nav.Language.Tests\bin\Debug\Nav.Language.Tests.dll" ".\Nav.Language.Extension.Tests\bin\Debug\Nav.Language.Extension.Tests.dll"
 
 REM Pause
