de6ee7e023a62e0fa2fa40f461178599747df328
IInspectable <github@smjh.de>
2018-09-18 21:44:09 +0200

    Refactoring

--- Nav.Language/FindReferences/FindReferences.cs
+++ Nav.Language/FindReferences/FindReferences.cs
@@ -5,6 +5,8 @@ using System.Collections.Immutable;
 using System.Threading.Tasks;
 using System.Linq;
 
+using JetBrains.Annotations;
+
 using Pharmatechnik.Nav.Language.Text;
 
 #endregion
@@ -32,36 +34,11 @@ namespace Pharmatechnik.Nav.Language.FindReferences {
                         return;
                     }
 
-                    if (reference.SyntaxTree == null) {
+                    var referenceItem = ToReferenceItem(definitionItem, reference);
+
+                    if (referenceItem == null) {
                         continue;
                     }
-
-                    var line = reference.SyntaxTree.SourceText.GetTextLineAtPosition(reference.Location.Start);
-
-                    var lineExtent = line.ExtentWithoutLineEndings;
-
-                    var lineParts = reference.SyntaxTree
-                                             .GetClassifiedText(lineExtent)
-                                             .ToImmutableArray();
-
-                    var lineHighlightExtent = new TextExtent(start: reference.Start - line.Start,
-                                                             length: reference.Location.Length);
-
-                    var previewExtent = GetPreviewExtent(line);
-
-                    var previewParts = reference.SyntaxTree
-                                                .GetClassifiedText(previewExtent)
-                                                .ToImmutableArray();
-
-                    var previewHighlightExtent = new TextExtent(start: reference.Start - previewExtent.Start,
-                                                                length: reference.Location.Length);
-
-                    var referenceItem = new ReferenceItem(definition: definitionItem,
-                                                 location: reference.Location,
-                                                 lineParts: lineParts,
-                                                 lineHighlightExtent: lineHighlightExtent,
-                                                 previewParts: previewParts,
-                                                 previewHighlightExtent: previewHighlightExtent);
 
                     await context.OnReferenceFoundAsync(referenceItem);
 
@@ -71,12 +48,48 @@ namespace Pharmatechnik.Nav.Language.FindReferences {
 
         }
 
+        [CanBeNull]
+        static ReferenceItem ToReferenceItem(DefinitionItem definitionItem, ISymbol reference) {
+
+            if (definitionItem == null || reference.SyntaxTree == null) {
+                return null;
+            }
+
+            var line = reference.SyntaxTree.SourceText.GetTextLineAtPosition(reference.Location.Start);
+
+            var lineExtent = line.ExtentWithoutLineEndings;
+
+            var lineParts = reference.SyntaxTree
+                                     .GetClassifiedText(lineExtent)
+                                     .ToImmutableArray();
+
+            var lineHighlightExtent = new TextExtent(start : reference.Start - line.Start,
+                                                     length: reference.Location.Length);
+
+            var previewExtent = GetPreviewExtent(line);
+
+            var previewParts = reference.SyntaxTree
+                                        .GetClassifiedText(previewExtent)
+                                        .ToImmutableArray();
+
+            var previewHighlightExtent = new TextExtent(start : reference.Start - previewExtent.Start,
+                                                        length: reference.Location.Length);
+
+            var referenceItem = new ReferenceItem(definition            : definitionItem,
+                                                  location              : reference.Location,
+                                                  lineParts             : lineParts,
+                                                  lineHighlightExtent   : lineHighlightExtent,
+                                                  previewParts          : previewParts,
+                                                  previewHighlightExtent: previewHighlightExtent);
+            return referenceItem;
+        }
+
         const int PreviewLinesOnOneSide = 3;
 
         static TextExtent GetPreviewExtent(SourceTextLine line) {
 
             var sourceText = line.SourceText;
-            if (sourceText.TextLines.Count == 1) {
+            if (sourceText.TextLines.Count <= 1) {
                 return line.ExtentWithoutLineEndings;
             }
 
