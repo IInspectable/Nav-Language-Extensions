e3b5bfb114341f7f6888a084fd60125625b5a7ba
IInspectable <github@smjh.de>
2019-03-06 20:58:12 +0100

    Build.bat: vs2019 preview build

--- Build.bat
+++ Build.bat
@@ -5,7 +5,7 @@ if "%config%" == "" (
    set config=Debug
 )
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /t:restore
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" Nav.Language.Extensions.sln /p:Configuration="%config%" /maxcpucount:4 /v:n
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\\MSBuild\Current\Bin\MSBuild.exe" /t:restore
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\\MSBuild\Current\Bin\MSBuild.exe" Nav.Language.Extensions.sln /p:Configuration="%config%" /maxcpucount:4 /v:n
 
 REM Pause
