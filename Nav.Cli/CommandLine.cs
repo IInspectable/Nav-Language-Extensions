9891b5f80f2bdf60fd8053bb531edd9ecd813903
IInspectable <github@smjh.de>
2022-10-06 13:23:05 +0200

    .

--- Nav.Cli/CommandLine.cs
+++ Nav.Cli/CommandLine.cs
@@ -8,34 +8,33 @@ using NDesk.Options;
 
 namespace Pharmatechnik.Nav.Language; 
 
-sealed class CommandLine {
+sealed record CommandLine {
    
     public CommandLine() {
         Sources = new List<string>();
     }
 
-    public string       Directory         { get; set; }
-    public List<string> Sources           { get; }
-    public bool         Force             { get; set; }
-    public bool         GenerateToClasses { get; set; }
-    public bool         UseSyntaxCache    { get; set; }
-    public bool         Verbose           { get; set; }
-    public bool         FullPaths         { get; set; }
-    public bool         NoWarnings        { get; set; }
+    public string       Directory            { get; private set; }
+    public List<string> Sources              { get; }
+    public bool         Force                { get; private set; }
+    public bool         GenerateToClasses    { get; private set; }
+    public bool         UseSyntaxCache       { get; private set; }
+    public bool         Verbose              { get; private set; }
+    public bool         FullPaths            { get; private set; }
+    public bool         NoWarnings           { get; private set; }
+    public string       ProjectRootDirectory { get; private set; }
+    public string       IwflRootDirectory    { get; private set; }
 
     public bool   Analyze { get; set; }
     public string Pattern { get; set; }
-
-    public string ProjectRootDirectory { get; set; }
-    public string IwflRootDirectory    { get; set; }
-
+    
     public static CommandLine Parse(string[] commandline) {
 
         bool        showHelp = false;
         CommandLine cla      = new CommandLine();
         var p = new OptionSet {
             { "d=|directory="       , "Alle .nav-Dateien im Verzeichnis und allen Unterverzeichnissen sind Eingabedateien.", v => cla.Directory = v },
-            { "s=|sources="         , ".nav-Eingabedatei", v => cla.Sources.Add(v) },
+            { "s=|sources="         , ".nav-Eingabedatei.", v => cla.Sources.Add(v) },
             { "f|force"             , "Überschreibt die Ausgabedatei(en) auch wenn sich diese nicht geändert haben.", v => cla.Force = v           != null },
             { "t|generatetoclasses" , "Erzeugt eine leere TO Klasse, wenn diese noch nicht exisitert.", v => cla.GenerateToClasses = v != null },
             { "c|useSyntaxCache"    , "Cached Syntaxen an statt sie immer wieder neu zu parsen.", v => cla.UseSyntaxCache = v             != null },
