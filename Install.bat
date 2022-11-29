438d6ff0514bf2b4339770162000c816e0e8f4d9
IInspectable <github@smjh.de>
2022-11-29 15:27:54 +0100

    Install.bat

--- Install.bat
+++ Install.bat
@@ -1,3 +1,2 @@
 @Echo Off
-powershell.exe -noprofile -Command "& {. '%~dp0Nav.Language.Extension2019\bin\Debug\Pharmatechnik.Nav.Language.Extension.2019.vsix'}"
 powershell.exe -noprofile -Command "& {. '%~dp0Nav.Language.Extension2022\bin\Debug\Pharmatechnik.Nav.Language.Extension.2022.vsix'}"
